551f27dfd84ae1296c7827f599ecea999834fb8b
Tobias Fox <tobiasfox@gmx.net>
2020-09-18 22:33:15 +0200

    update finding overlapping algorithmus

--- SpriteSortingPlugin/Assets/SpriteSorting/Editor/SpriteSortingUtility.cs
+++ SpriteSortingPlugin/Assets/SpriteSorting/Editor/SpriteSortingUtility.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 using Object = UnityEngine.Object;
 
 namespace SpriteSorting
@@ -15,19 +16,20 @@ namespace SpriteSorting
 
             //TODO: consider prefab scene
             var spriteRenderers = Object.FindObjectsOfType<SpriteRenderer>();
-            var filteredSpriteRenderers = new List<SpriteRenderer>();
 
-            foreach (var currentRenderer in spriteRenderers)
+            var sortingGroupParents = new Dictionary<int, SortingGroup[]>();
+            foreach (var spriteRenderer in spriteRenderers)
             {
-                if (data.selectedLayers.Contains(currentRenderer.sortingLayerID))
+                var sortingGroups = spriteRenderer.GetComponentsInParent<SortingGroup>();
+                if (sortingGroups.Length > 0)
                 {
-                    filteredSpriteRenderers.Add(currentRenderer);
+                    sortingGroupParents.Add(spriteRenderer.GetInstanceID(), sortingGroups);
                 }
             }
 
-            foreach (var filteredSpriteRenderer in filteredSpriteRenderers)
+            foreach (var spriteRendererToCheck in spriteRenderers)
             {
-                if (CheckOverlappingSprites(data.cameraProjectionType, filteredSpriteRenderers, filteredSpriteRenderer,
+                if (CheckOverlappingSprites(data, spriteRenderers, sortingGroupParents, spriteRendererToCheck,
                     out List<OverlappingItem> overlappingSprites))
                 {
                     result.overlappingItems = overlappingSprites;
@@ -38,31 +40,87 @@ namespace SpriteSorting
             return result;
         }
 
-        private static bool CheckOverlappingSprites(CameraProjectionType cameraProjectionType,
-            List<SpriteRenderer> filteredSpriteRenderers, SpriteRenderer spriteRendererToCheck,
+        private static bool CheckOverlappingSprites(SpriteSortingData data,
+            SpriteRenderer[] allSpriteRenderers, Dictionary<int, SortingGroup[]> sortingGroupParents,
+            SpriteRenderer spriteRendererToCheck,
             out List<OverlappingItem> overlappingSprites)
         {
             overlappingSprites = new List<OverlappingItem>();
-            Debug.Log("start search in " + filteredSpriteRenderers.Count + " sprite renderers for an overlap with " +
+            Debug.Log("start search in " + allSpriteRenderers.Length + " sprite renderers for an overlap with " +
                       spriteRendererToCheck.name);
-            foreach (var filteredSpriteRenderer in filteredSpriteRenderers)
+
+            var sortingLayerToCheck = spriteRendererToCheck.sortingLayerID;
+            var sortingOrderToCheck = spriteRendererToCheck.sortingOrder;
+
+            var isSortingGroupContained = sortingGroupParents.TryGetValue(spriteRendererToCheck.GetInstanceID(),
+                out var sortingGroupsOfSpriteRendererToCheck);
+            if (isSortingGroupContained)
             {
-                if (filteredSpriteRenderer == null || filteredSpriteRenderer == spriteRendererToCheck ||
-                    filteredSpriteRenderer.sortingOrder != spriteRendererToCheck.sortingOrder ||
-                    !filteredSpriteRenderer.bounds.Intersects(spriteRendererToCheck.bounds))
+                var outMostSortingGroup =
+                    sortingGroupsOfSpriteRendererToCheck[sortingGroupsOfSpriteRendererToCheck.Length - 1];
+                sortingLayerToCheck = outMostSortingGroup.sortingLayerID;
+                sortingOrderToCheck = outMostSortingGroup.sortingOrder;
+            }
+
+            foreach (var spriteRenderer in allSpriteRenderers)
+            {
+                if (spriteRenderer == spriteRendererToCheck ||
+                    !spriteRenderer.bounds.Intersects(spriteRendererToCheck.bounds))
                 {
                     continue;
                 }
 
-                if (cameraProjectionType == CameraProjectionType.Orthogonal &&
-                    Math.Abs(filteredSpriteRenderer.transform.position.z - spriteRendererToCheck.transform.position.z) >
+                if (data.cameraProjectionType == CameraProjectionType.Orthogonal &&
+                    Math.Abs(spriteRenderer.transform.position.z - spriteRendererToCheck.transform.position.z) >
                     Tolerance)
                 {
                     continue;
                 }
 
-                overlappingSprites.Add(
-                    new OverlappingItem(filteredSpriteRenderer));
+                var spriteRendererHasSortingGroupParents =
+                    sortingGroupParents.TryGetValue(spriteRenderer.GetInstanceID(), out var sortingGroups);
+
+                if (!spriteRendererHasSortingGroupParents || !isSortingGroupContained)
+                {
+                    //sprite has no SortingGroup in parent
+                    if (!data.selectedLayers.Contains(sortingLayerToCheck) ||
+                        spriteRenderer.sortingLayerID != sortingLayerToCheck ||
+                        spriteRenderer.sortingOrder != sortingOrderToCheck)
+                    {
+                        continue;
+                    }
+
+                    overlappingSprites.Add(new OverlappingItem(spriteRenderer));
+                    continue;
+                }
+
+                //sprite has SortingGroups in parents
+
+                SortingGroup sortingGroupSpriteRendererToCheck;
+                SortingGroup sortingGroupSpriteRenderer;
+
+                for (int i = sortingGroupsOfSpriteRendererToCheck.Length - 1, j = sortingGroups.Length - 1;
+                    i >= 0 && j >= 0;
+                    i--, j--)
+                {
+                    sortingGroupSpriteRendererToCheck = sortingGroupsOfSpriteRendererToCheck[i];
+                    sortingGroupSpriteRenderer = sortingGroups[j];
+
+                    if (sortingGroupSpriteRendererToCheck != null && sortingGroupSpriteRenderer != null &&
+                        sortingGroupSpriteRendererToCheck == sortingGroupSpriteRenderer)
+                    {
+                        continue;
+                    }
+                }
+
+                // filteredSpriteRenderer.sortingOrder != spriteRendererToCheck.sortingOrder
+                // data.selectedLayers.
+
+                // if (sortingGroup != null && sortingGroup.sortingLayerID != spriteRendererToCheck.sortingLayerID &&
+                //     sortingGroup.sortingOrder != spriteRendererToCheck.sortingOrder)
+                // {
+                //     continue;
+                // }
             }
 
             if (overlappingSprites.Count <= 0)
