cad36aff07ec97ff0fae7872be9bc5833f3ccf8c
Tobias Fox <tobiasfox@gmx.net>
2020-11-18 21:24:31 +0100

    update position criteria implementation

--- SpriteSortingPlugin/Assets/SpriteSortingPlugin/Editor/AutomaticSorting/Criterias/PositionSortingCriterion.cs
+++ SpriteSortingPlugin/Assets/SpriteSortingPlugin/Editor/AutomaticSorting/Criterias/PositionSortingCriterion.cs
@@ -17,30 +17,19 @@ namespace SpriteSortingPlugin.AutomaticSorting.Criterias
         {
             var results = new int[2];
 
+            if (autoSortingCalculationData.cameraProjectionType == CameraProjectionType.Orthographic)
+            {
+                return results;
+            }
+
             var spriteRendererTransform = autoSortingComponent.spriteRenderer.transform;
             var otherSpriteRendererTransform = otherAutoSortingComponent.spriteRenderer.transform;
-            var isAutoSortingComponentCloser = false;
             var cameraTransform = autoSortingCalculationData.cameraTransform;
-            switch (autoSortingCalculationData.cameraProjectionType)
-            {
-                case CameraProjectionType.Perspective:
 
-                    var perspectiveDistance = CalculatePerspectiveDistance(spriteRendererTransform, cameraTransform);
-                    var otherPerspectiveDistance =
-                        CalculatePerspectiveDistance(otherSpriteRendererTransform, cameraTransform);
-                    isAutoSortingComponentCloser = perspectiveDistance <= otherPerspectiveDistance;
-
-                    break;
-                case CameraProjectionType.Orthographic:
-                    var orthographicDistance = CalculateOrthographicDistance(spriteRendererTransform, cameraTransform);
-                    var otherOrthographicDistance =
-                        CalculateOrthographicDistance(otherSpriteRendererTransform, cameraTransform);
-                    isAutoSortingComponentCloser = orthographicDistance <= otherOrthographicDistance;
-
-                    break;
-                case CameraProjectionType.Both:
-                    break;
-            }
+            var perspectiveDistance = CalculatePerspectiveDistance(spriteRendererTransform, cameraTransform);
+            var otherPerspectiveDistance =
+                CalculatePerspectiveDistance(otherSpriteRendererTransform, cameraTransform);
+            var isAutoSortingComponentCloser = perspectiveDistance <= otherPerspectiveDistance;
 
             if (PositionSortingCriterionData.isFurtherAwaySpriteInForeground)
             {
@@ -62,19 +51,6 @@ namespace SpriteSortingPlugin.AutomaticSorting.Criterias
         private float CalculatePerspectiveDistance(Transform spriteRendererTransform, Transform cameraTransform)
         {
             var distance = spriteRendererTransform.position - cameraTransform.position;
-            // distance = distance * -0.5f + transform.position;
-
-            return distance.magnitude;
-        }
-
-        private float CalculateOrthographicDistance(Transform spriteRendererTransform, Transform cameraTransform)
-        {
-            var positionOnCameraPlane =
-                new Vector3(spriteRendererTransform.position.x, spriteRendererTransform.position.y,
-                    cameraTransform.position.z);
-            var distance = spriteRendererTransform.position - positionOnCameraPlane;
-            // distance = distance * -0.5f + transform.position;
-
             return distance.magnitude;
         }
     }
