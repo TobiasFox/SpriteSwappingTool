a036d82034d50fb2bf4571545d21a4145773c879
Tobias Fox <tobiasfox@gmx.net>
2020-12-18 12:37:52 +0100

    update finish screen

--- SpriteSortingPlugin/Assets/SpriteSortingPlugin/Editor/Survey/UI/Wizard/SurveySteps/FinishingSurvey.cs
+++ SpriteSortingPlugin/Assets/SpriteSortingPlugin/Editor/Survey/UI/Wizard/SurveySteps/FinishingSurvey.cs
@@ -1,4 +1,5 @@
-﻿using SpriteSortingPlugin.Survey.UI.Wizard.Data;
+﻿using System.Collections.Generic;
+using SpriteSortingPlugin.Survey.UI.Wizard.Data;
 using SpriteSortingPlugin.UI;
 using UnityEditor;
 using UnityEngine;
@@ -7,18 +8,30 @@ namespace SpriteSortingPlugin.Survey.UI.Wizard
 {
     public class FinishingSurvey : SurveyStep
     {
+        private const float FireworkLabelHeight = 256;
+        private const float CurrentSpriteWidth = 200;
+        private const float SpriteDisplayTime = 0.045f;
+        private const float ProgressbarSpeed = 0.45f;
+
+        private const string FinishingSpritePathAndName = "Assets/Sprites/Confetti_small.png";
+        // "Assets/SpriteSortingPlugin/Editor/Survey/UI/Wizard/SurveySteps/Confetti_0.png";
+
         private UserData userData;
-
-        private const string FinishingSpritePathAndName =
-            "Assets/SpriteSortingPlugin/Editor/Survey/UI/Wizard/SurveySteps/firework2.jpg";
-
-        private Sprite finishingSprite;
         private bool isSendingDataButtonPressed;
         private bool wasDataSendingSuccessfull;
-        private float progressValue;
+
+        private bool isEditorUpdateMethodAdded;
+
         private double lastEditorTime;
-        private bool isUpdatingProgressbarDelegatedAdded;
-        private float progressbarSpeed = 0.45f;
+        private float progressValue;
+        private bool isUpdatingProgressbar;
+
+        private bool isChangingFireWorkSprite;
+        private float remainingTimeToChangeSprite;
+        private List<Sprite> fireWorkSprites = new List<Sprite>();
+        private int currentSpriteIndex;
+        private Sprite currentSprite;
+        private Texture2D spriteTexture;
 
         public FinishingSurvey(string name, UserData userData) : base(name)
         {
@@ -27,25 +40,37 @@ namespace SpriteSortingPlugin.Survey.UI.Wizard
 
         public override void DrawContent()
         {
-            // if (finishingSprite == null)
-            // {
-            //     LoadFinishingSprite();
-            // }
-            //
-            //
-            // var spritePosition = new Rect(0, 0, 300, 300);
-            // EditorGUI.DrawTextureTransparent(spritePosition, finishingSprite.texture, ScaleMode.ScaleToFit);
+            if (fireWorkSprites.Count == 0)
+            {
+                LoadFinishingSprite();
+            }
+            
+            using (new EditorGUILayout.HorizontalScope())
+            {
+                using (new EditorGUILayout.VerticalScope(GUILayout.Height(FireworkLabelHeight)))
+                {
+                    GUILayout.FlexibleSpace();
+                    var labelStyle = new GUIStyle(Styling.QuestionLabelStyle) {alignment = TextAnchor.MiddleCenter};
+                    labelStyle.fontSize++;
+                    EditorGUILayout.LabelField(
+                        "Thank you very much for participating in this survey.\nYour input helps me very much!",
+                        labelStyle);
+                    GUILayout.FlexibleSpace();
+                }
 
-            progressbarSpeed = OwnGUIHelper.DrawField(progressbarSpeed);
+                GUILayout.FlexibleSpace();
+
+                using (new EditorGUILayout.VerticalScope(GUILayout.Height(FireworkLabelHeight)
+                ))
+                {
+                    GUILayout.FlexibleSpace();
+                    DrawCurrentSprite();
+                    GUILayout.FlexibleSpace();
+                }
+            }
 
             using (new EditorGUI.IndentLevelScope())
             {
-                EditorGUILayout.LabelField(
-                    "Thank you very much for participating in this survey. Your input helps me very much!",
-                    Styling.QuestionLabelStyle);
-
-                GUILayout.Label("picture");
-                EditorGUILayout.Space();
                 EditorGUILayout.LabelField(
                     "If you want to be informed about the results or to keep updated, you can optionally fill in your mail address:",
                     Styling.LabelWrapStyle);
@@ -53,74 +78,144 @@ namespace SpriteSortingPlugin.Survey.UI.Wizard
                 userData.mailAddress = EditorGUILayout.TextField("Your Email", userData.mailAddress);
 
                 EditorGUILayout.Space(30);
-                EditorGUILayout.LabelField("And now the final step: sending the data.", Styling.LabelWrapStyle);
                 EditorGUILayout.LabelField(
                     "Please keep this window open and make sure the PC is connected to the internet.",
                     Styling.LabelWrapStyle);
+            }
 
-
-                using (new EditorGUI.DisabledScope(isSendingDataButtonPressed))
+            using (new EditorGUI.DisabledScope(isSendingDataButtonPressed))
+            {
+                if (GUILayout.Button("Send data", GUILayout.Height(EditorGUIUtility.singleLineHeight * 2)))
                 {
-                    if (GUILayout.Button("Send data", GUILayout.Height(EditorGUIUtility.singleLineHeight * 2)))
-                    {
-                        isSendingDataButtonPressed = true;
-                        lastEditorTime = EditorApplication.timeSinceStartup;
-
-                        isUpdatingProgressbarDelegatedAdded = true;
-                        EditorApplication.update += UpdateProgressbar;
-                    }
-                }
-
-                var progressbarRect = EditorGUILayout.GetControlRect();
-                EditorGUI.ProgressBar(progressbarRect, progressValue, "");
-
-                if (wasDataSendingSuccessfull)
-                {
-                    EditorGUILayout.LabelField("The data was successfully sent!", Styling.LabelWrapStyle);
-                    EditorGUILayout.Space(15);
-                    EditorGUILayout.LabelField("You can now close this window.", Styling.LabelWrapStyle);
-                }
-
-                if (GUILayout.Button("Debug: successful send Data"))
-                {
-                    UpdateUIAfterSuccessfullySendData();
+                    StartSendingData();
                 }
             }
+
+            var progressbarRect = EditorGUILayout.GetControlRect();
+            EditorGUI.ProgressBar(progressbarRect, progressValue, "");
+
+            if (wasDataSendingSuccessfull)
+            {
+                var labelWrapStyle = new GUIStyle(Styling.LabelWrapStyle) {alignment = TextAnchor.MiddleCenter};
+                EditorGUILayout.LabelField("The data was successfully sent!", labelWrapStyle);
+                EditorGUILayout.Space(15);
+                EditorGUILayout.LabelField("You can now close this window.", labelWrapStyle);
+            }
+
+            if (GUILayout.Button("Debug: successful send Data"))
+            {
+                UpdateUIAfterSuccessfullySendData();
+            }
+        }
+
+        public override void CleanUp()
+        {
+            RemoveEditorUpdate();
+        }
+
+        private void StartSendingData()
+        {
+            isSendingDataButtonPressed = true;
+            lastEditorTime = EditorApplication.timeSinceStartup;
+            isUpdatingProgressbar = true;
+
+            AddEditorUpdate();
+        }
+
+        private void DrawCurrentSprite(float width = 0)
+        {
+            var rect = GUILayoutUtility.GetRect(CurrentSpriteWidth, CurrentSpriteWidth);
+
+            if (Event.current.type != EventType.Repaint)
+            {
+                return;
+            }
+
+            var spriteRect = currentSprite.rect;
+            spriteRect.xMin /= spriteTexture.width;
+            spriteRect.xMax /= spriteTexture.width;
+            spriteRect.yMin /= spriteTexture.height;
+            spriteRect.yMax /= spriteTexture.height;
+            GUI.DrawTextureWithTexCoords(rect, spriteTexture, spriteRect);
         }
 
         private void UpdateUIAfterSuccessfullySendData()
         {
             wasDataSendingSuccessfull = true;
-            isUpdatingProgressbarDelegatedAdded = false;
+            isUpdatingProgressbar = false;
             progressValue = 1;
-            EditorApplication.update -= UpdateProgressbar;
         }
 
-        private void UpdateProgressbar()
+        private void EditorUpdate()
         {
-            var scaledEditorDeltaTime =
-                (float) (EditorApplication.timeSinceStartup - lastEditorTime) * progressbarSpeed;
-
-            progressValue += scaledEditorDeltaTime;
-            if (progressValue > 1)
+            if (isChangingFireWorkSprite)
             {
-                progressValue = 0;
+                if (EditorApplication.timeSinceStartup > remainingTimeToChangeSprite)
+                {
+                    remainingTimeToChangeSprite = (float) (EditorApplication.timeSinceStartup + SpriteDisplayTime);
+                    currentSpriteIndex = (currentSpriteIndex + 1) % fireWorkSprites.Count;
+                    currentSprite = fireWorkSprites[currentSpriteIndex];
+                }
             }
 
-            lastEditorTime = EditorApplication.timeSinceStartup;
-        }
-
-        public override void CleanUp()
-        {
-            if (isUpdatingProgressbarDelegatedAdded)
+            if (isUpdatingProgressbar)
             {
-                EditorApplication.update -= UpdateProgressbar;
+                var scaledEditorDeltaTime =
+                    (float) (EditorApplication.timeSinceStartup - lastEditorTime) * ProgressbarSpeed;
+
+                progressValue += scaledEditorDeltaTime;
+                if (progressValue > 1)
+                {
+                    progressValue = 0;
+                }
+
+                lastEditorTime = EditorApplication.timeSinceStartup;
             }
         }
 
         private void LoadFinishingSprite()
         {
-            // finishingSprite = AssetDatabase.LoadAssetAtPath<Sprite>(FinishingSpritePathAndName);
+            var allAssets = AssetDatabase.LoadAllAssetsAtPath(FinishingSpritePathAndName);
+            foreach (var asset in allAssets)
+            {
+                switch (asset)
+                {
+                    case Sprite fireworkSprite:
+                        fireWorkSprites.Add(fireworkSprite);
+                        break;
+                    case Texture2D texture:
+                        spriteTexture = texture;
+                        break;
+                }
+            }
+
+            currentSprite = fireWorkSprites[currentSpriteIndex];
+            isChangingFireWorkSprite = true;
+            remainingTimeToChangeSprite = (float) (EditorApplication.timeSinceStartup + SpriteDisplayTime);
+
+            AddEditorUpdate();
+        }
+
+        private void AddEditorUpdate()
+        {
+            if (isEditorUpdateMethodAdded)
+            {
+                return;
+            }
+
+            isEditorUpdateMethodAdded = true;
+            EditorApplication.update += EditorUpdate;
+        }
+
+        private void RemoveEditorUpdate()
+        {
+            if (!isEditorUpdateMethodAdded)
+            {
+                return;
+            }
+
+            EditorApplication.update -= EditorUpdate;
+            isEditorUpdateMethodAdded = false;
         }
     }
 }
